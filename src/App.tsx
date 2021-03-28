3a648f072cbaa42f7942ebefadbf5d7a03913653
RafaelRossales <rapha2704@gmail.com>
2021-03-28 12:06:37 -0300

    Creating basic components

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Dashboard } from './Components/Dashboard';
+import { Header } from './Components/Header';
+import { GlobalStyle } from './styles/global'
+
+export function App() {
+  return (
+    <>
+      <Header/>
+      <Dashboard/>
+      <GlobalStyle/>
+    </>
+  );
+}
+
